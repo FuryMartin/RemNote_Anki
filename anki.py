3a18b67a0396f1636ba93e97c9e6c55c174bace1
FuryMartin <41051953+FuryMartin@users.noreply.github.com>
2021-05-11 00:34:02 +0800

    anki.py

---
+++ anki.py
@@ -0,0 +1,46 @@
+import json
+import urllib.request
+import sys
+import win32clipboard as wc
+import win32con
+
+def request(action, **params):
+    """组合请求参数"""
+    return {'action': action, 'params': params, 'version': 6}
+
+def invoke(action, **params):
+    """"进行请求"""
+    requestJson = json.dumps(request(action, **params)).encode('utf-8')
+    response = json.load(urllib.request.urlopen(
+        urllib.request.Request('http://localhost:8765', requestJson)))
+    if len(response) != 2:
+        raise Exception('response has an unexpected number of fields')
+    if 'error' not in response:
+        raise Exception('response is missing required error field')
+    if 'result' not in response:
+        raise Exception('response is missing required result field')
+    if response['error'] is not None:
+        raise Exception(response['error'])
+    return response['result']
+
+def get_clipbox():
+    """获取剪贴板内容"""
+    wc.OpenClipboard()
+    text = wc.GetClipboardData(win32con.CF_TEXT)
+    wc.CloseClipboard()
+    return text.decode('GBK')
+
+text = get_clipbox()
+reference_id =sys.argv[1]
+my_notes = []
+cur_note = {
+    "deckName": "测试",#将这里改成你要加入的牌组名
+    "modelName": "RemNote",#将这里改成你的卡片类型
+    "fields": {
+    }
+}
+cur_note["fields"]["正面"] = text
+cur_note["fields"]["背面"] = reference_id
+my_notes.append(dict(cur_note))
+invoke("addNotes", notes=my_notes)
+
